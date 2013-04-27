34a9d81af29dd6718eb1d63816f7a8c3d456e2e0
Romain Lalaut <romain.lalaut@laposte.net>
2013-04-27 14:19:42 +0200

    small fix

--- app/helpers/product_image_helper.rb
+++ app/helpers/product_image_helper.rb
@@ -2,11 +2,11 @@ module ProductImageHelper
   def link_to_main_product_image(product)
     large_image_url = product.images.empty? ? "noimage/product.jpg" :
                       product.images.first.attachment.url(:large)
-                      link_to product_image(product), ActionController::Base.asset_host+large_image_url, :title => product.name
+                      link_to product_image(product), "#{ActionController::Base.asset_host}#{large_image_url}", :title => product.name
   end
 
   def link_to_image(image, title='')
-    link_to image_tag(image.url(:mini)), ActionController::Base.asset_host+image.url(:large), :title => title
+    link_to image_tag(image.url(:mini)), "#{ActionController::Base.asset_host}#{image.url(:large)}", :title => title
   end
 end
 
